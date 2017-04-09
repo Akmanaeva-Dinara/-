3827b29fe26827ee829ae0590f0d7a1ee6f91a33
Dinara_Damilevna <msakmanaeva77@gmail.com>
2017-04-10 00:07:10 +0500

    Условные операторы

--- Высокий.py
+++ Высокий.py
@@ -1,6 +1,14 @@
 import math
-c=input("скорость=(t^3)-((3t)^2)+2")
-p=input("производная скорости=(3t^2)-6t") 
-t=int(input("введите время="))
-S=((3*t)**2)-6*t
-print("Скорость тела=", S)
+a=float(input("введите а, не равное 0= "))
+b=float(input("введите b= "))
+c=float(input("введите c= "))
+d = b**2 - 4*a*c
+if d> 0:
+	x1 = (-b + math.sqrt(d))/(2*a)
+	x2 = (-b - math.sqrt(d))/(2*a)
+	print("x1 =",x1, "x2 =", x2)
+elif d == 0:
+	x = -b/(2*a)
+	print("x =", x)
+else:
+	print("Корней нет")
