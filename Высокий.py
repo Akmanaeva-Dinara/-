620620fd5a37c398d1356e77159c3341e8d268b7
Dinara_Damilevna <msakmanaeva77@gmail.com>
2017-04-10 00:08:42 +0500

    Программирование линейных алгоритмов

--- Высокий.py
+++ Высокий.py
@@ -1,14 +1,6 @@
 import math
-a=float(input("введите а, не равное 0= "))
-b=float(input("введите b= "))
-c=float(input("введите c= "))
-d = b**2 - 4*a*c
-if d> 0:
-	x1 = (-b + math.sqrt(d))/(2*a)
-	x2 = (-b - math.sqrt(d))/(2*a)
-	print("x1 =",x1, "x2 =", x2)
-elif d == 0:
-	x = -b/(2*a)
-	print("x =", x)
-else:
-	print("Корней нет")
+c=input("скорость=(t^3)-((3t)^2)+2")
+p=input("производная скорости=(3t^2)-6t") 
+t=int(input("введите время="))
+S=((3*t)**2)-6*t
+print("Скорость тела=", S)
--- Средний.py
+++ Средний.py
@@ -1,21 +1,8 @@
-import math
-x=int(input("x="))
-y=int(input("y="))
-s1=0
-s2=0
-if ((x>0) and (y>0)):
-   s1=x
-   s2=y
-elif ((x<0) and (y>0)):
-            s1=x*(-1)
-            s2=y
-elif ((x>0) and (y<0)):
-              s2=y*(-1)
-              s1=x
-elif (x<0) and (y<0):
-             s1=x*(-1)
-             s2=y*(-1)
-p=(s1+s2)/2
-q=math.sqrt(s1*s2)
-print('Полусумма равна ',p);
-print('Квадратный корень из произведения равен ',q);
+import math 
+x,b=3.5,0.4
+a=math.log(x)
+c=(a**2)+math.sqrt(b*x)
+y=math.exp(2*x)+9**c
+print("a =", a)
+print("c =", c)
+print("y =", y)
