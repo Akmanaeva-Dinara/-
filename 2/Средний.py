1d245af3ada22fb0390a043fb12c0ba0ad9e83b6
Dinara_Damilevna <msakmanaeva77@gmail.com>
2017-04-17 23:25:27 +0500

    логические выражения

---
+++ 2/Средний.py
@@ -0,0 +1,9 @@
+A=int(input("a="))
+B=int(input("b="))
+C=int(input("c="))
+if ((A%2==0) or (B%2==0)):
+	print(True)
+else: print(False)
+if ((A%3==0) and(B%3==0) and(C%3==0)):
+	print(True)
+else: print(False)
